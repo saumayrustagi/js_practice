64835ae2086d95decc7721d59976111a5d6b2f23
saumayrustagi <saumay03pro@gmail.com>
2025-08-02 16:47:48 +0530

    change vite base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/js_practice/', // <-- Add this line
+  base: './', // <-- Add this line
   build: {
     outDir: 'docs', // This is the default, but good to be explicit
   },
